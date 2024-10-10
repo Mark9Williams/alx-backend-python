6bbdcbcb4a0bc742ab0a514c78a5619e1bca2599
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-10-10 18:12:42 +0000

    checks

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 def floor(n: float) -> int:
     """Return the floor of the float"""
     return int(n)
--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 def to_str(n: float) -> str:
     """Convert a float to a string"""
     return str(n)
