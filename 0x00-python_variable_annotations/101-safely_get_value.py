47fa62474782fc8cfff938f8afc38dd9fe01cb32
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-10-10 21:10:46 +0000

    comment in function

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -10,6 +10,7 @@ def safely_get_value(
         dct: Mapping,
         key: Any,
         default: Union[T, None] = None) -> Union[Any, T]:
+    """Safely return the value of a key in a dictionary"""
     if key in dct:
         return dct[key]
     else:
