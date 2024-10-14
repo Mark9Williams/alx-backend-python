88f03f9fcfb116c316d03434e043879fccf7c869
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-10-14 20:57:09 +0000

    task_4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python3
-"""Asynchronous coroutine that returns a list of delays"""
+"""Module to execute multiple asyncio.Tasks using task_wait_random"""
 
 import asyncio
 from typing import List
 import importlib.util
 
 # Dynamically load the module with unconventional name
-spec_concurrent = importlib.util.spec_from_file_location(
-    "concurrent_coroutines", "./3-tasks.py")
-module = importlib.util.module_from_spec(spec_concurrent)
-spec_concurrent.loader.exec_module(module)
+spec = importlib.util.spec_from_file_location(
+    "wait_random", "./0-basic_async_syntax.py")
+module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(module)
 
 # Now you can access wait_random from the loaded module
-wait_random = module.task_wait_random
+wait_random = module.wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """takes an integer max_delay and returns an asyncio.Task."""
+    return asyncio.create_task(wait_random(max_delay))
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Spawns task_wait_random n times with the specified max_delay."""
-    tasks = [task_wait_n(n, max_delay) for _ in range(n)]
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
 
     # Gather results as tasks complete
